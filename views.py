f16e47ec05519f00f3f700a7f4c04d0bda46e2a5
happyxiao <614838577@qq.com>
2019-06-11 05:55:59 -0700

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     '''首页'''
     return HttpResponse('index')
+
+
+def login(request):
+    '''重定向'''
+    return redirect('/index')
